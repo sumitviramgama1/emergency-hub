76eeb9327662b07e6711108c7075a0b18944741d
sumit viramgama <sumitviramgama1@gmail.com>
2025-04-08 03:58:58 +0530

    mmy

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-// const cors = require("cors");
+const cors = require("cors");
 require("dotenv").config();
 const geminiRoutes = require("./routes/geminiRoute");
 const locationRoutes = require("./routes/locationRoute");
