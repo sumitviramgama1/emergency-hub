9225bf1951ed4929202db4d8eb12de9707af0c8c
sumit viramgama <sumitviramgama1@gmail.com>
2025-04-08 03:52:36 +0530

    my

--- backend/routes/geminiRoute.js
+++ backend/routes/geminiRoute.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { getGeminiResponse } = require("../controllers/geminiController");
 
+
 const router = express.Router();
 
 router.post("/chat", getGeminiResponse);
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const cors = require("cors");
+// const cors = require("cors");
 require("dotenv").config();
 const geminiRoutes = require("./routes/geminiRoute");
 const locationRoutes = require("./routes/locationRoute");
@@ -13,12 +13,12 @@ const app = express();
 connectDB();
 app.use(bodyParser.json());
 app.use(express.json());
-app.use(cors({
-  origin: ['https://emergency-hub-kxyn.vercel.app', 'http://localhost:3000'],
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
-app.options('*', cors()); // Enable preflight requests for all routes
+// app.use(cors({
+//   origin: ['https://emergency-hub-kxyn.vercel.app', 'http://localhost:3000'],
+//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//   allowedHeaders: ['Content-Type', 'Authorization']
+// }));
+// app.options('*', cors()); // Enable preflight requests for all routes
 
 app.use("/api/auth", authRoutes);
 app.use("/api/gemini", geminiRoutes);
